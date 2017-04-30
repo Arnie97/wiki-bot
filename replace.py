700f49c113c1e389a21a8b07f9c479a898a27130
Arnie97 <arnie97@gmail.com>
2017-04-30 12:31:36 +0800

    Handles keyword highlighting and HTML entities.

--- replace.py
+++ replace.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
+import html
 import mwclient
+import colorama
 
 site = mwclient.Site('test.wikipedia.org', clients_useragent='Arnie97-Bot')
 site.login()
 
 for item in site.search('nice'):
-    print(item['title'], item['size'])
-    print(item['snippet'])
+    print(''.join((
+        colorama.Fore.CYAN,
+        item['title'],
+        colorama.Fore.GREEN,
+        ' (', str(item['size']), ')',
+        colorama.Fore.RESET
+    )))
+    print(html.unescape(
+        item['snippet']
+        .replace('<span class="searchmatch">', colorama.Fore.MAGENTA)
+        .replace('</span>', colorama.Fore.RESET)
+    ))
+    input()
