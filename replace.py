a0afdb6b01d35263479a6165b85aa6c153b02f16
Arnie97 <arnie97@gmail.com>
2017-04-30 14:06:28 +0800

    Prompt the user to pick a choice.

--- replace.py
+++ replace.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import html
 import mwclient
 import colorama
@@ -9,6 +10,7 @@ site.login()
 
 for item in site.search('nice'):
     print(''.join((
+        '\n',
         colorama.Fore.CYAN,
         item['title'],
         colorama.Fore.GREEN,
@@ -20,4 +22,17 @@ for item in site.search('nice'):
         .replace('<span class="searchmatch">', colorama.Fore.MAGENTA)
         .replace('</span>', colorama.Fore.RESET)
     ))
-    input()
+
+    action = input(''.join((
+        colorama.Fore.YELLOW,
+        'Replace? [Y/n/q]: ',
+        colorama.Fore.RESET
+    ))).lower()
+    if not action:
+        action = 'y'
+
+    {
+        'n': lambda: None,
+        'y': lambda: print(item),
+        'q': lambda: sys.exit()
+    }[action]()
